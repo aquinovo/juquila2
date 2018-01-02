d4a7ed6966968260740e03906d3f18a4ac59bed5
aquinovo <vela.aquino@gmail.com>
2018-01-01 22:06:55 -0600

    subir videos

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -148,3 +148,11 @@ $(document).ready(function() {
     }
   }
 })
+
+$(document).ready(function(){
+  $('ul.first').bsPhotoGallery({
+    "classes" : "col-lg-4 col-md-4 col-sm-6 col-xs-12 col-xxs-12",
+    "hasModal" : true,
+    // "fullHeight" : false
+  });
+});
