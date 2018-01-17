9eaac133509a7f4211b09ef74b5da5e478a33fc0
aquinovo <vela.aquino@gmail.com>
2018-01-17 10:12:22 -0600

    mensaje

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var users = require('./routes/users');
+
+
 var app = express();
 
 // view engine setup
@@ -18,6 +21,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/users', users);
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,4 +1,46 @@
 
- function mostrar(){
+function mostrar(){
   document.getElementById('formulario').style.display = 'block';
-}	
+}
+
+function datos(){
+	var dato=document.getElementById("datos");
+	 window.open("homilia.html?usuario="+dato[0].value+"&contrasena="+dato[1].value+"&url="+dato[2].value);
+}
+
+$(document).ready(function() {
+    var txtFile = "http://localhost:3000/url.txt"
+	var file = new File(txtFile);
+
+	file.open("r"); // open file with read access
+	var str = "";
+	while (!file.eof) {
+		// read each line of text
+		str += file.readln() + "\n";
+	}
+	file.close(); 
+
+	console.log(str);
+
+	var usuario=getParameterByName("usuario");
+	var contrasena=getParameterByName("contrasena");
+	var url=getParameterByName("url");
+
+	if(usuario=="santuario" && contrasena=="juquila"){
+		console.log("usuario corecto");
+		var link= document.getElementById("referencia");
+	link.setAttribute("href",url);
+	}
+
+
+});
+
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
