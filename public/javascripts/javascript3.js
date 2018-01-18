993d5c4b1b99a81e82ffe30c8a2f492a37903ccc
aquinovo <vela.aquino@gmail.com>
2018-01-18 02:28:49 -0600

    link4

---
+++ public/javascripts/javascript3.js
@@ -0,0 +1,36 @@
+function mostrar(){
+	document.getElementById('formulario').style.display = 'block';
+}
+
+window.onload = function(){
+
+	$.ajax({
+		url: 'users/evangelio'
+	}).then(function(data) {
+		console.log(data);
+		console.log(window.location);
+		url.setAttribute("href",data);
+		url.innerHTML="";  
+		url.innerHTML="<h3>Click para ver el Evangelio<h3>";  
+	});
+
+	//url.innerHTML="";
+
+} 
+
+$("#enviar").click(function(){
+	var dato=document.getElementById("datos");
+	console.log(dato[2].value);
+	$.post( "users/evangelio", { usuario: dato[0].value, contrasena: dato[1].value,link: dato[2].value })
+	.done(function( data ) {
+		console.log( "Data Loaded: " + data );
+		window.location="evangelio.html";
+	});
+});
+
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
--- routes/users.js
+++ routes/users.js
@@ -56,9 +56,36 @@ arquidiocesis2=function(req, res) {
 	  }
 	});
 };
+evangelio1=function(req, res) {
+	console.log(req.body); 
+	if(req.body.usuario=="santuario" && req.body.contrasena=="virgenjuquila"){	
+		fs.writeFile("./evangelio.txt", req.body.link, function (err) {
+		    if (err) {
+		    	return console.log(err);
+		    }
+		    res.send('true');
+		});
+	}else{
+		res.send('false');
+	}
+	res.send('true');
+};
+evangelio2=function(req, res) {
+	fs.readFile('./evangelio.txt', 'utf-8', (err, data) => {
+	  if(err) {
+	    console.log('error: ', err);
+	    res.send("false");
+	  } else {
+	    console.log(data);
+	    res.send(data);
+	  }
+	});
+};
 router.get('/',get);
 router.post('/homilia',homilia1);
 router.get('/homilia',homilia2);
 router.post('/arquidiocesis',arquidiocesis1);
 router.get('/arquidiocesis',arquidiocesis2);
+router.post('/evangelio',evangelio1);
+router.get('/evangelio',evangelio2);
 module.exports = router;
